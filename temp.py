71bf7356043295074e4fb0d7ca7b93b7e1210b6f
Jason Cheng <dumplinglife7584@gmail.com>
2023-09-30 20:13:09 -0400

    batched read and running

--- temp.py
+++ temp.py
@@ -8,43 +8,38 @@ from tqdm import tqdm
 # load video
 parser = argparse.ArgumentParser()
 parser.add_argument("--video_path", required=True)
-parser.add_argument("--num_frames", required=True)
+parser.add_argument("--num_frames", required=True, type=int)
 args = parser.parse_args()
 
 VIDEO_PATH = args.video_path
-NUM_FRAMES = int(args.num_frames)
+NUM_FRAMES = args.num_frames
 
-cap = cv2.VideoCapture(VIDEO_PATH)
-frames = []
-for i in tqdm(range(NUM_FRAMES)):
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, (224, 224))
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = frame / 255.0
-    frames.append(frame)
-
-
-cap.release()
-
-video = np.array(frames)
-video = np.transpose(video, (3, 0, 1, 2))
-video = video.reshape((-1, 3, 32, 224, 224))
-video = torch.from_numpy(video).float()
-
-print(video.shape)
-
+if NUM_FRAMES % 32 != 0:
+    raise ValueError("NUM_FRAMES must be a multiple of 32!")
 
 net = S3D('s3d_dict.npy', 512)
 net.load_state_dict(torch.load('s3d_howto100m.pth'))
-
 net = net.eval()
-BATCH_SIZE = 1
 
+cap = cv2.VideoCapture(VIDEO_PATH)
 outputs = []
-for i in tqdm(range(0, video.size(0), BATCH_SIZE)):
-    video_chunk = video[i:i+BATCH_SIZE]
+for i in tqdm(range(NUM_FRAMES//32)):
+    frames = []
+    for j in range(32):
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (224, 224))
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = frame / 255.0
+        frames.append(frame)
+    video = np.array(frames)
+    video = np.transpose(video, (3, 0, 1, 2))
+    video = video.reshape((-1, 3, 32, 224, 224))
+    video = torch.from_numpy(video).float()
     with torch.no_grad():
-        output_chunk = net(video_chunk)
+        output_chunk = net(video)
     outputs.append(output_chunk["video_embedding"])
+
+cap.release()
+
 video_output_embedding = torch.cat(outputs, dim=0)
 print(video_output_embedding.shape)
